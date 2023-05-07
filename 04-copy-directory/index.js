ea985707504326d5a7e8cf48384ca329311b9a7c
checas1 <checas104@gmail.com>
2023-05-07 15:12:30 +0300

    feat: add silution to fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const path = require('path');
+const fs = require('fs');
 
+async function directoryCopy() {
+  await fs.promises.rm(path.join(__dirname, 'files-copy'), {
+    recursive: true,
+    force: true,
+  });
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  const filesDirectory = await fs.promises.readdir(
+    path.join(__dirname, 'files'),
+    {
+      withFileTypes: true,
+    }
+  );
+  for (let item of filesDirectory) {
+    fs.promises.copyFile(
+      path.join(path.join(__dirname, 'files'), item.name),
+      path.join(path.join(__dirname, 'files-copy'), item.name)
+    );
+  }
+}
+directoryCopy();
